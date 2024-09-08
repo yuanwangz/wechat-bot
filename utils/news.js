1ede00e05fc2f6c08282cefa0df2e4f9cdae011e
yuanwangz <yuanwan2021@gmail.com>
2024-09-08 16:20:26 +0800

    热搜文本格式调整

--- utils/news.js
+++ utils/news.js
@@ -40,7 +40,8 @@ export async function get_news(msgContent) {
                 // 检查是否有 "热度"
                 const heat = item.more ? `   热度: ${item.more}` : '';
                 // 拼接结果字符串
-                res += `${count}. ${item.title} [${t.toISOString().replace('T', ' ').substring(0, 19)}]${heat}\nhttps://www.anyknew.com/go/${item.iid}\n\n`;
+                // res += `${count}. ${item.title} [${t.toISOString().replace('T', ' ').substring(0, 19)}]${heat}\nhttps://www.anyknew.com/go/${item.iid}\n\n`;
+                res += `${count}. ${item.title} ${heat}\nhttps://www.anyknew.com/go/${item.iid}\n`;
                 count++;
             }
         }
