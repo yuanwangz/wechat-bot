e223d11aae9e3ace68a10df475fb989253e0b1dc
yuanwangz <yuanwan2021@gmail.com>
2024-09-13 01:29:52 +0800

    fix bugs

--- utils/hanyuxinjie.js
+++ utils/hanyuxinjie.js
@@ -119,7 +119,25 @@ export async function get_hyxj(msgContent) {
         // 保存图片
         await writeFile(imagePath, jpgBuffer);
 
-        return imagePath;
+        const saveFileDir = path.join(path.resolve('./WeChat Files/file'), md5, filename);
+        let file_dir = path.dirname(saveFileDir);
+        if (!fs.existsSync(file_dir)) {
+            // 如果目标目录不存在，创建它
+            fs.mkdirSync(file_dir, { recursive: true });
+        }
+        fs.copyFileSync(imagePath, saveFileDir);
+        // 延迟1分钟后删除文件
+        setTimeout(() => {
+            fs.unlink(imagePath, (err) => {
+                if (err) {
+                    console.error('删除本地文件错误:', err);
+                } else {
+                    console.log('本地文件已删除:', imagePath);
+                }
+            });
+        }, 10000);
+
+        return filename;
     } catch (error) {
         console.error('Error occurred:', error);
         return '';
